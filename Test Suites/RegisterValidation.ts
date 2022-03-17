5f362659d88edae7045d148db70d8ddb59f7a86a
farisya.ahdin <farisya.ahdin@tanihub.com>
2022-03-17 22:27:45 +0700

    add test

---
+++ Test Suites/RegisterValidation.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RegisterValidation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>805df15e-26ea-420a-9cda-7ab35ba46aea</testSuiteGuid>
+   <testCaseLink>
+      <guid>7d2195b4-adc5-4cc6-9ed3-af910f9922ec</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/ValidateRegisterFailed</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aaebfd57-358d-4d7d-8a58-1543e144d1fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/ValidateRegisterSuccess</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/UserValidation.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserValidation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8995d78d-fd18-4f7f-a23a-47013805d13a</testSuiteGuid>
+   <testCaseLink>
+      <guid>38b04417-a8da-4f40-bc51-f51139b9a132</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User/ValidateUserLists</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>46c6830d-42f0-45ac-81c3-18e6cb8ab384</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User/ValidateUserNotFound</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>78cc71ec-107b-4768-a89d-c3af9d9a9956</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User/ValidateUserDetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
