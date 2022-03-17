66487ee1315126782f39233210a15352bfc0d159
ahdinfm28 <farisyahdin@gmail.com>
2022-03-17 22:42:29 +0700

    add login tc

---
+++ Test Suites/LoginValidation.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginValidation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9159fa62-63ff-4447-b21c-985d8bbb2f3d</testSuiteGuid>
+   <testCaseLink>
+      <guid>25284193-568e-4dcf-8d71-09f8ec377e90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/ValidateLoginFailed</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a5174c4a-8ca0-4143-a3ff-3eccf3c6119b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/ValidateLoginSuccess</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
